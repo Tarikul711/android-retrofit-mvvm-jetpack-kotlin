4b6b106cf12ed200df7273c49cc2189ba35b23a4
Tarikul711 <Tarikul711@gmail.com>
2020-09-06 10:33:05 +0600

    Init the project

---
+++ app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tos.android_retrofit_mvvm_jetpack_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
