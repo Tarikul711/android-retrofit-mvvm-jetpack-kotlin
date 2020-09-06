a32481da6fd3e901a93d85bbfac0570f082bc2f2
Tarikul711 <Tarikul711@gmail.com>
2020-09-06 14:12:22 +0600

    Make the product list ViewModel

--- app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/ui/main/viewmodel/ProductListViewModel.kt
+++ app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/ui/main/viewmodel/ProductListViewModel.kt
@@ -1,9 +1,22 @@
 package com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.main.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.tos.android_retrofit_mvvm_jetpack_kotlin.data.repository.ProductListRepository
+import com.tos.myapplication.utils.Resource
+
 /**
  *Created by tarikul on 6/9/20
  */
 
-class ProductListViewModel (){
+class ProductListViewModel(private val productListRepository: ProductListRepository) : ViewModel() {
 
+    private fun getProducts(apiKey: String) = liveData {
+        emit(Resource.loading(data = null))
+        try {
+            emit(Resource.success(data = productListRepository.getProducts(apiKey)))
+        } catch (exception: Exception) {
+            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
+        }
+    }
 }
