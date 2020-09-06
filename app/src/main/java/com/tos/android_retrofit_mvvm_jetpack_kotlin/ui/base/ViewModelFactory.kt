cc7d7a6cef332b78c2270e5cb75f2f3bc16af209
Tarikul711 <Tarikul711@gmail.com>
2020-09-06 14:42:32 +0600

    Add base ViewModelFactory class for the project

---
+++ app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/ui/base/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tos.android_retrofit_mvvm_jetpack_kotlin.data.repository.ProductListRepository
+import com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.main.viewmodel.ProductListViewModel
+import com.tos.myapplication.data.api.ApiHelper
+import java.lang.IllegalArgumentException
+
+/**
+ *Created by tarikul on 5/9/20
+ */
+class ViewModelFactory(private val apiHelper: ApiHelper) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProductListViewModel::class.java)) {
+            return ProductListViewModel(ProductListRepository(apiHelper)) as T
+        }
+        throw IllegalArgumentException("Unknown class name")
+    }
+}
--- app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/ui/main/view/MainActivity.kt
@@ -2,12 +2,32 @@ package com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.tos.android_retrofit_mvvm_jetpack_kotlin.R
+import com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.main.adapter.ProductListAdapter
+import com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.main.viewmodel.ProductListViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: ProductListViewModel
+    private lateinit var adapter: ProductListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViewModel()
+        setupUI()
+        setupObservers()
+    }
+
+    private fun setupViewModel() {
+
+    }
+
+    private fun setupUI() {
+
+    }
+
+    private fun setupObservers() {
+
     }
 }
