d9b1fec6fa7605937c10b4037f2876da80457615
Tarikul711 <Tarikul711@gmail.com>
2020-09-08 10:25:45 +0600

    rewrite the OffersActivity.

---
+++ app/src/main/java/com/tos/android_retrofit_mvvm_jetpack_kotlin/ui/main/view/OffersActivity.kt
@@ -0,0 +1,20 @@
+package com.tos.android_retrofit_mvvm_jetpack_kotlin.ui.main.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tos.android_retrofit_mvvm_jetpack_kotlin.R
+
+/**
+ *Created by tarikul on 8/9/20
+ */
+
+class OffersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offers)
+        /* setupViewModel()
+         setupUI()
+         setupObservers()*/
+    }
+}
